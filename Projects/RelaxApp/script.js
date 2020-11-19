fd699e2328e8b350fe3dec7a5609fcb85e7641d3
Anton Mikaskin <garrakombo@gmail.com>
2020-11-19 00:41:57 -0800

    finished Relax App

--- Projects/RelaxApp/script.js
+++ Projects/RelaxApp/script.js
@@ -1,98 +1,77 @@
+const startbutton = document.querySelector('.start')
+const endTimerMessage = document.querySelector('.end-timer-message')
 const circle = document.querySelector('.progress-ring__circle')
-const radius = circle.r.baseVal.value
-const circumference = radius * 2 * Math.PI
 const relaxSound = document.querySelector('audio')
 const playButton = document.querySelector('.icon')
 const timer = document.querySelector('.timer')
-const startMinutes = 1
-let totalTime = 60 //startMinutes * 60
+const answers = endTimerMessage.querySelector('.answers')
+const successMessage = endTimerMessage.querySelector('.success')
+const question = endTimerMessage.querySelector('.question')
+
+const radius = circle.r.baseVal.value
+const circumference = radius * 2 * Math.PI
+const startMinutes = 5
+let totalTime = startMinutes * 60
+
+
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`
+circle.style.strokeDashoffset = `${circumference}`
+
+
+startbutton.addEventListener('click', () => {
+  startbutton.style.display = 'none'
+  startTimer()
+  relaxSound.play()
+})
+
+answers.addEventListener('click', (evt) => {
+  
+  if(evt.target.textContent == 'Yes') {
+    successMessage.style.display = 'block'
+    question.style.display = 'none'
+    answers.style.display = 'none'
+  } else if(evt.target.textContent == 'No') {
+    endTimerMessage.style.display = 'none'
+    relaxSound.play()
+    startTimer()
+  }
+  
+})
 
 function setProgress(percent) {
   const offset = circumference - percent / 100 * circumference;
   circle.style.strokeDashoffset = offset
 }
 
-circle.style.strokeDasharray = `${circumference} ${circumference}`
-circle.style.strokeDashoffset = `${circumference}`
+function startTimer() {
+  let percents = 0
+  let time = totalTime
+  const onePercent = +(100 / totalTime).toFixed(2)
+  let currentPercent = 0
 
-// setProgress(98)
+  let interval = setInterval(() => {
+    const minutes = Math.floor(time / 60)
+    let seconds = time % 60
 
-// input.addEventListener('change', function(e) {
-//   if (input.value < 101 && input.value > -1) {
-//     setProgress(input.value);
-//   }  
-// })
+    seconds = seconds < 10 ? '0' + seconds : seconds 
 
-// playButton.addEventListener('click', function() {
-  
-// })
+    if(time < 0) {
+      clearInterval(interval)
 
-// let timerId = setInterval(function () {
-//   seconds = MINUTES * 60 // Получаем секунды
-//   minutes = MINUTES / 60 % 60 // Получаем минуты
-//   // Условие если время закончилось то...
-//   if (minutes <= 0) {
-//       // Таймер удаляется
-//       clearInterval(timerId);
-//       // Выводит сообщение что время закончилось
-//       alert("Время закончилось");
-//   } else { // Иначе
-//       // Создаём строку с выводом времени
-//       let strTimer = `${Math.trunc(minutes)}:${seconds}`;
-//       // Выводим строку в блок для показа таймера
-//       timer.textContent = strTimer;
-//   }
-//   --minutes; // Уменьшаем таймер
-// }, 1000)
+      timer.textContent = ''
+      endTimerMessage.style.display = 'flex'
 
-let percents = 0
-let currentPercent = totalTime
-let time = totalTime
-
-let interval = setInterval(() => {
-  const minutes = Math.floor(time / 60)
-  let seconds = time % 60
-
-  seconds = seconds < 10 ? '0' + seconds : seconds 
-
-  if(time < 0) {
-    clearInterval(interval)
-    alert('done!')
-  } else {
-    showProgress(time)
-    timer.textContent = `${Math.trunc(minutes)}:${seconds}`
-    time--
-  }
-  
-}, 1000);
-
-function showProgress(time) {
-  const onePercent = totalTime / 100
-  
-  if(time < currentPercent) {
-    currentPercent = +(currentPercent - onePercent).toFixed(1)
-    percents++
-    setProgress(percents)
-    console.log(percents, time, currentPercent)
-  } else {
-    // setProgress(percents)
-    console.log('hhhhhh')
-  }
+      relaxSound.pause();
+      relaxSound.currentTime = 0;
+    } else {
+      currentPercent > 100 ? setProgress(100) : setProgress(currentPercent)
+      currentPercent += onePercent
+      
+      timer.textContent = `${Math.trunc(minutes)}:${seconds}`
+      
+      time--
+    }
+    
+  }, 1000);
 }
-
-
-
-// let num = 0
-// let count = 10
-
-// let interval = setInterval(() => {
-//   if(count <= 0) {
-//     clearInterval(interval)
-//   } else {
-//     num = num + 0.6
-//     count--
-//     console.log(Math.round(num))
-//   }
-  
-  
-// }, 1000);
