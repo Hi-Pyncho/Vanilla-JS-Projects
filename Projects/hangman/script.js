9373eae638119cb8dc99697a612d348616c30abe
Anton Mikaskin <garrakombo@gmail.com>
2020-12-26 21:58:31 +0300

    добавил функцию gameEnd

--- Projects/hangman/script.js
+++ Projects/hangman/script.js
@@ -2,7 +2,7 @@ const wrongLettersEl = document.querySelector('.wrong-letters-container')
 const wordEl = document.querySelector('.word-container')
 const popup = document.querySelector('.popup-container')
 const playAgainBtn = document.querySelector('.play-button')
-const fintalMessage = document.querySelector('.final-message')
+const finalMessage = document.querySelector('.final-message')
 const notification = document.querySelector('.notification-container')
 
 const figureParts = document.querySelectorAll('.figure-part')
@@ -10,8 +10,10 @@ const figureParts = document.querySelectorAll('.figure-part')
 const words = ['application', 'programming', 'interface', 'wizard']
 
 let selectedWord = words[Math.floor(Math.random() * words.length)]
+// let selectedWord = 'wigard'
 
-const correctLetters = []
+// const correctLetters = ['w', 'i', 'g', 'a', 'r', 'd']
+const correctLetters = ['w', 'i', 'g', 'a', 'r', 'd']
 const wrongLetters = []
 
 function displayWords() {
@@ -26,6 +28,17 @@ function displayWords() {
       .join('')
     }
   `
+
+  
+}
+
+function gameEnd() {
+  const innerWord = wordEl.innerText.replace(/\n/g, '')
+  
+  if(innerWord === selectedWord) {
+    finalMessage.innerHTML = `Congratulations!<br> You won!`
+    popup.style.display = 'flex'
+  }
 }
 
 displayWords()
