65745019a1efe26b087f1c34332cfe08ae8934a1
Anton Mikaskin <garrakombo@gmail.com>
2021-01-03 22:58:34 +0300

    добавил в js основные переменные

---
+++ Projects/typeGame/script.js
@@ -0,0 +1,11 @@
+const bulletContainer = document.querySelector('.text-bullets')
+const textInput = document.querySelector('.word')
+const gameTime = document.querySelector('.time')
+const gameScore = document.querySelector('.score')
+const enemy = document.querySelector('.enemy')
+const firstRevolver = document.querySelector('.revolver1')
+const secondRevolver = document.querySelector('.revolver2')
+
+const wordLength = 11
+let score = 0
+let time = 0
